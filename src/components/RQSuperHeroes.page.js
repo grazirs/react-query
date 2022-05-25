177113172a7dc483d1dd58ef95ac2092243e85b6
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-05-25 10:40:26 -0300

    refactor and add handling errors

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -5,16 +5,16 @@ const fetchSuperHeroes = () => {
   return axios.get('http://localhost:4000/superheroes')
 }
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery('super-heroes',fetchSuperHeroes)
+  const { isLoading, data, isError, error} = useQuery('super-heroes',fetchSuperHeroes)
   
-  if (isLoading) {
-    return <h2>Loading...</h2>
+  if (isError) {
+    return <h2>{error.message}</h2>
   }
 
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
-      {data?.data.map((hero) => {
+      { isLoading? (<h2>Loading...</h2>) : data?.data.map(hero => {
         return <div key={hero.name}>{hero.name}</div>
       })}
     </>
