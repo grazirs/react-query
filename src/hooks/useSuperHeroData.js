b5b9f76b9de55c85ded5eed28acabd8a24315f94
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-05-25 15:56:52 -0300

    refactor fetchSuperHero

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,10 +1,11 @@
 import { useQuery } from "react-query"
 import axios from 'axios'
 
-const fetchSuperHero = (heroId) => {
+const fetchSuperHero = ({ queryKey }) => {
+  const heroId = queryKey[1]
   return axios.get(`http://localhost:4000/superheroes/${heroId}`)
 }
 
 export const useSuperHeroData = (heroId) => {
-  return useQuery(['super-hero', heroId ], () => fetchSuperHero(heroId))
+  return useQuery(['super-hero', heroId ], fetchSuperHero)
 }
