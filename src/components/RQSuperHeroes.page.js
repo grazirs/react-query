ad707f7243c6515ab18ecb05a9ab3a1830ba0a9e
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-05-25 14:43:25 -0300

    add page for super hero detail

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,4 +1,5 @@
 import { useSuperHeroesData } from "../hooks/useSuperHeroesData"
+import { Link } from "react-router-dom"
 
 export const RQSuperHeroesPage = () => {
   const { isLoading, data, isError, error, isFetching, refetch } = useSuperHeroesData()
@@ -12,8 +13,11 @@ export const RQSuperHeroesPage = () => {
       <h2>RQ Super Heroes Page</h2>
       <button onClick={refetch}>Fetch heroes</button>
       {isLoading ? (<h2>Loading...</h2>) : data?.data.map(hero => {
-        return <div key={hero.name}>{hero.name}</div>
-      })}
+        return (
+          <div key={hero.id}>
+            <Link to={`/rq-super-heroes/${hero.id}`}>{hero.name}</Link>
+          </div>
+      )})}
     </>
   )
 }
