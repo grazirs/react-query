8e053bae7093c8626a7533a73d055cb032387fee
Graziele Ribeiro <graziribeiro95@gmail.com>
2022-05-26 10:23:52 -0300

    add parallel, dynamic and dependent queries

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { HomePage } from './components/Home.page'
 import { RQSuperHeroesPage } from './components/RQSuperHeroes.page'
 import { SuperHeroesPage } from './components/SuperHeroes.page'
 import { RQSuperHeroPage } from './components/RQSuperHeroPage'
+import { ParallelQueriesPage } from './components/ParallelQueries.page'
+import { DynamicParallelPage } from './components/DynamicParallel.page'
+import { DependentQueriesPage } from './components/DependentQueries.page'
 
 const queryClient = new QueryClient()
 
@@ -32,6 +35,9 @@ function App() {
             <Route path='/super-heroes' element={<SuperHeroesPage />} />
             <Route path='/rq-super-heroes' element={<RQSuperHeroesPage />} />
             <Route path='/rq-super-heroes/:heroId' element={<RQSuperHeroPage />} />
+            <Route path='/rq-parallel' element={<ParallelQueriesPage />} />
+            <Route path='/rq-dynamic-parallel' element={<DynamicParallelPage heroIds={[1, 3]} />} />
+            <Route path='/rq-dependent' element={<DependentQueriesPage email='vishwas@example.com' />} />
           </Routes>
         </div>
       </Router>
